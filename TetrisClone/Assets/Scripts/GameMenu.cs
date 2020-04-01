28a11207e00dff70db0f362b1e5cdb59c4a81714
Luis <luisgomezruiz@gmail.com>
2020-04-01 15:44:18 +0200

    Don't know this things but ok

---
+++ TetrisClone/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+	public Text levelText;
+
+	public Text highScoreText;
+	public Text highScoreText2;
+	public Text highScoreText3;
+
+
+	private void Start()
+	{
+		levelText.text = "0";
+
+		highScoreText.text = (PlayerPrefs.GetInt("highScore")).ToString();
+
+		highScoreText2.text = (PlayerPrefs.GetInt("highScore2")).ToString();
+
+		highScoreText3.text = (PlayerPrefs.GetInt("highScore3")).ToString();
+		
+	}
+
+	public void  Play()
+	{
+		if (Game.startingLevel==0)
+		{
+			Game.startingAtLevelZero = true;
+		}
+		else
+		{
+			Game.startingAtLevelZero = false;
+		}
+
+		SceneManager.LoadScene("Level");
+	}
+	public void ChangedValue (float Value)
+	{
+		Game.startingLevel = (int)Value;
+		levelText.text = Value.ToString();
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
---
+++ TetrisClone/Assets/Scripts/GhostTetromino.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTetromino : MonoBehaviour
+{
+    
+    void Start()
+    {
+		tag = "CurrentGhostTetromino";
+
+		foreach (Transform mino in transform)
+		{
+			mino.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f,.2f);
+		}
+    }
+
+    
+    void Update()
+    {
+		FollowActiveTetromino();
+		MoveDown();
+    }
+
+	void FollowActiveTetromino()
+	{
+		Transform currentActiveTetrominoTransform = GameObject.FindGameObjectWithTag("CurrentActiveTetromino").transform;
+
+		transform.position = currentActiveTetrominoTransform.position;
+		transform.rotation = currentActiveTetrominoTransform.rotation;
+
+	}
+
+	void MoveDown()
+	{
+		while (CheckIsValidPosittion())
+		{
+			transform.position += new Vector3(0, -1, 0);
+		}
+
+		if (!CheckIsValidPosittion())
+		{
+			transform.position += new Vector3(0, 1, 0);
+		}
+	}
+
+	bool CheckIsValidPosittion()
+	{
+		foreach (Transform mino in transform)
+		{
+			Vector2 position = FindObjectOfType<Game>().Round(mino.position);
+
+			if (FindObjectOfType<Game>().CheckIsInsideGrid(position)==false)
+				return false;
+
+			if (FindObjectOfType<Game>().GetTransformAtGridPosition(position) != null && FindObjectOfType<Game>().GetTransformAtGridPosition(position).parent.tag == "CurrentActiveTetromino")
+				return true;
+			if (FindObjectOfType<Game>().GetTransformAtGridPosition(position)!=null && FindObjectOfType<Game>().GetTransformAtGridPosition(position).parent!=transform)
+				return false;
+		}
+
+		return true;
+	}
+
+}
